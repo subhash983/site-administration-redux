6cd973735099c42d233b67bd6413803ee24eae20
subhash983 <subhash.983@gmail.com>
2017-06-20 22:00:29 +0530

    adding production build scripts

---
+++ tools/build.js
@@ -0,0 +1,32 @@
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import colors from 'colors';
+
+/* eslint-disable no-console */
+
+process.env.NODE_ENV = 'production';
+
+console.log('Generating minified bundle for production via webpack.This will take a moment.');
+
+webpack(webpackConfig).run((err, stats) => {
+    if (err) {
+        console.log(err.bold.red);
+        return 1;
+    }
+
+    const jsonStats = stats.toJson();
+
+    if (jsonStats.hasErrors) {
+        return jsonStats.errors.map(error => console.log(error.red));
+    }
+
+    if (jsonStats.hasWarnings) {
+        console.log('Webpack generated the following warnings'.bold.yellow);
+        jsonStats.warnings.map(warning => console.log(warning.yellow));
+    }
+
+    console.log(`Webpack Stats: ${stats}`);
+
+    console.log('your app has been compiled in production mode and written to /dist');
+    return 0;
+});
